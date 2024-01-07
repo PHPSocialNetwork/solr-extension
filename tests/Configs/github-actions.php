5fcd4d0b9f88dabe8f0ca5bfc48c46394dbe6223
Georges <contact@geolim4.com>
2024-01-07 13:32:10 +0100

    Simplified test config object

--- tests/Configs/github-actions.php
+++ tests/Configs/github-actions.php
@@ -2,10 +2,10 @@
 
 use Phpfastcache\Drivers\Solr\Config as SolrConfig;
 
-return (fn(SolrConfig $config) => $config->setItemDetailedDate(true)
+return (new SolrConfig())
+    ->setItemDetailedDate(true)
     ->setCoreName('phpfastcache')
     ->setPort(8983)
     ->setHost('127.0.0.1')
     ->setPath('/')
-    ->setScheme('http')
-)(new SolrConfig());
+    ->setScheme('http');
